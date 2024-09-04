ff7b69693d0374bebf6f42e99b5227dd1df5d1f2
Mariana Dias <marianamkt6@gmail.com>
2024-09-04 11:01:11 -0300

    Application Layouts

--- PizzaShopWeb/src/Router.tsx
+++ PizzaShopWeb/src/Router.tsx
@@ -1,9 +1,38 @@
 
 import{createBrowserRouter} from 'react-router-dom'
+import { AppLayout } from './pages/_layouts/app'
 import { Dashboard } from './pages/app/dashboard'
+import { AuthLayout } from './pages/_layouts/auth'
 import { SignIn } from './pages/auth/sign-in'
+//import { Dashboard } from './pages/app/dashboard'
+//import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
-    {path:'/', element: <Dashboard/>},
-    {path: '/sign-in', element:<SignIn/> },
+
+{
+    path:'/',
+    element: <AppLayout/>,
+
+    children:[{ //sub-rota, como o AppLayout e o Dashboard possuem o mesmo 'patch' só será mostrado o que foi definido primeiro, então para mostrar mais conteudo, precisar ter uma sub-rota.
+        path:'/',
+        element: <Dashboard/>,
+    }]
+},
+
+{
+    path:'/',
+    element: <AuthLayout/>,
+
+    children:[{ //sub-rota, como o AppLayout e o Dashboard possuem o mesmo 'patch' só será mostrado o que foi definido primeiro, então para mostrar mais conteudo, precisar ter uma sub-rota.
+        path:'/sign-in',
+        element: <SignIn/>,
+    }]
+}
+
+
+
+
+    //{path:'/', element: <Dashboard/>},
+    
+   // {path: '/sign-in', element:<SignIn/> },
 ])
